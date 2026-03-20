1d7d4acfacafbe84b64675ebf74880c8960489f8
Matthias Trinkl <matthias.trinkl@gmail.com>
2026-03-21 00:07:45 +0100

    Add SonarQube Gradle plugin
    Register the SonarQube Gradle plugin in the root build.gradle.kts (org.sonarqube v4.4.1.3373) by adding it to the plugins block. This makes SonarQube available for running static code analysis across the project and its subprojects.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ktlint) apply false
+    id("org.sonarqube") version "4.4.1.3373"
 }
 
 subprojects {
