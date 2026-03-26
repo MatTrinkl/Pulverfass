7b1589f6d3957c1b1ebf55a1c47f94aff77f230f
Matthias Trinkl <matthias.trinkl@gmail.com>
2026-03-26 20:59:25 +0100

    Fix Linkt and update Editorconfig

--- shared/src/test/kotlin/at/aau/pulverfass/shared/network/MessageHeaderTest.kt
+++ shared/src/test/kotlin/at/aau/pulverfass/shared/network/MessageHeaderTest.kt
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class MessageHeaderTest {
 
