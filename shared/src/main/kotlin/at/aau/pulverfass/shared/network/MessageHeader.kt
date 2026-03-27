efdf5f2a60c336df64c8d0005e833b3bdcd76e08
Matthias Trinkl <matthias.trinkl@gmail.com>
2026-03-27 17:09:51 +0100

    Fix Lint

--- shared/src/main/kotlin/at/aau/pulverfass/shared/network/MessageHeader.kt
+++ shared/src/main/kotlin/at/aau/pulverfass/shared/network/MessageHeader.kt
@@ -1,7 +1,7 @@
 package at.aau.pulverfass.shared.network
 
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.descriptors.PrimitiveKind
 import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
 import kotlinx.serialization.descriptors.SerialDescriptor
