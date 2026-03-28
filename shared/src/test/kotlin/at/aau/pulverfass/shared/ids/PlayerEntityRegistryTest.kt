e23215160a3c6a1bce9c85457522b50acb8048a1
FirstJakob <j1gruber@edu.aau.at>
2026-03-28 16:28:43 +0100

    test: add tests for player-entity mapping

---
+++ shared/src/test/kotlin/at/aau/pulverfass/shared/ids/PlayerEntityRegistryTest.kt
@@ -0,0 +1,31 @@
+package at.aau.pulverfass.shared.ids
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class PlayerEntityRegistryTest {
+
+    @Test
+    fun `player should get assigned entities`() {
+        val playerId = IdFactory.nextPlayerId()
+        val entityId = IdFactory.nextEntityId()
+
+        PlayerEntityRegistry.addEntity(playerId, entityId)
+
+        val entities = PlayerEntityRegistry.getEntities(playerId)
+
+        assertTrue(entities.contains(entityId))
+    }
+
+    @Test
+    fun `entity should map back to player`() {
+        val playerId = IdFactory.nextPlayerId()
+        val entityId = IdFactory.nextEntityId()
+
+        PlayerEntityRegistry.addEntity(playerId, entityId)
+
+        val foundPlayer = PlayerEntityRegistry.getPlayer(entityId)
+
+        assertEquals(playerId, foundPlayer)
+    }
+}
