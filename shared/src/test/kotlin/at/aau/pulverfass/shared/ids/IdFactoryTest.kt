523ebeeb888b3591a0b87992a2ee04f430cb0d34
FirstJakob <j1gruber@edu.aau.at>
2026-03-28 16:09:42 +0100

    test: add tests for id uniqueness in id factory

---
+++ shared/src/test/kotlin/at/aau/pulverfass/shared/ids/IdFactoryTest.kt
@@ -0,0 +1,15 @@
+package at.aau.pulverfass.shared.ids
+
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Test
+
+class IdFactoryTest {
+
+    @Test
+    fun `ids should be unique`() {
+        val id1 = IdFactory.nextPlayerId()
+        val id2 = IdFactory.nextPlayerId()
+
+        assertNotEquals(id1, id2)
+    }
+}
