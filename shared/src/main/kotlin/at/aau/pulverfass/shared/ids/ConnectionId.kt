8e2c4d8bc57b40494fb6793b621a14c0da2b582d
FirstJakob <j1gruber@edu.aau.at>
2026-03-29 15:36:34 +0200

    feat: add KDoc and validation to shared id classes

--- shared/src/main/kotlin/at/aau/pulverfass/shared/ids/ConnectionId.kt
+++ shared/src/main/kotlin/at/aau/pulverfass/shared/ids/ConnectionId.kt
@@ -2,6 +2,16 @@ package at.aau.pulverfass.shared.ids
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Eindeutige ID für eine technische Verbindung.
+ *
+ * Diese ID wird serverseitig vergeben und kann über das Netzwerk übertragen werden.
+ */
+
 @Serializable
 @JvmInline
-value class ConnectionId(val value: Long)
+value class ConnectionId(val value: Long) {
+    init {
+        require(value > 0) { "ConnectionId muss positiv sein, war aber $value." }
+    }
+}
--- shared/src/main/kotlin/at/aau/pulverfass/shared/ids/PlayerId.kt
+++ shared/src/main/kotlin/at/aau/pulverfass/shared/ids/PlayerId.kt
@@ -2,6 +2,16 @@ package at.aau.pulverfass.shared.ids
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Eindeutige ID für einen Spieler.
+ *
+ * Diese ID wird serverseitig vergeben und kann über das Netzwerk übertragen werden.
+ */
+
 @Serializable
 @JvmInline
-value class PlayerId(val value: Long)
+value class PlayerId(val value: Long) {
+    init {
+        require(value > 0) { "PlayerId muss positiv sein, war aber $value." }
+    }
+}
