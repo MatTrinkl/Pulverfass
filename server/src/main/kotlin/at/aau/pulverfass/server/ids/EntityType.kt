e149227755454c99d5175f23f545690c30443c2e
FirstJakob <j1gruber@edu.aau.at>
2026-03-31 22:31:18 +0200

    feat: add EntityType enum adapted for Risiko domain

---
+++ server/src/main/kotlin/at/aau/pulverfass/server/ids/EntityType.kt
@@ -0,0 +1,15 @@
+package at.aau.pulverfass.server.ids
+
+/**
+ * Beschreibt die aktuell verwendeten Entity-Typen fuer Risiko.
+ *
+ * PLAYER:
+ * - repraesentiert einen Spieler
+ *
+ * TERRITORY:
+ * - repraesentiert ein Gebiet auf der Karte
+ */
+enum class EntityType {
+    PLAYER,
+    TERRITORY,
+}
