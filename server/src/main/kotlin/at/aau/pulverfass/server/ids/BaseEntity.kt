41bd6e5ae048cda156170d7c6e0e5d3a8159ea05
FirstJakob <j1gruber@edu.aau.at>
2026-03-31 22:32:28 +0200

    feat: introduce BaseEntity as common entity structure

---
+++ server/src/main/kotlin/at/aau/pulverfass/server/ids/BaseEntity.kt
@@ -0,0 +1,14 @@
+package at.aau.pulverfass.server.ids
+
+import at.aau.pulverfass.shared.ids.EntityId
+
+/**
+ * Gemeinsame Basis fuer alle Entities im Spiel.
+ *
+ * Jede Entity besitzt eine eindeutige EntityId
+ * und einen festen EntityType.
+ */
+abstract class BaseEntity(
+    open val entityId: EntityId,
+    open val entityType: EntityType,
+)
