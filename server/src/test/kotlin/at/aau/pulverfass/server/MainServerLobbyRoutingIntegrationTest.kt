6a7fb80bcb9bb2a21085a487933642a92c830ffa
FirstJakob <j1gruber@edu.aau.at>
2026-05-26 16:33:40 +0200

    Test: wait for routing hook in multi-lobby routing test

--- server/src/test/kotlin/at/aau/pulverfass/server/MainServerLobbyRoutingIntegrationTest.kt
+++ server/src/test/kotlin/at/aau/pulverfass/server/MainServerLobbyRoutingIntegrationTest.kt
@@ -821,6 +821,7 @@ class MainServerLobbyRoutingIntegrationTest {
 
                     waitUntilProcessed(lobbyManager, lobbyA, expectedCount = 2)
                     waitUntilProcessed(lobbyManager, lobbyB, expectedCount = 1)
+                    waitUntilCounter(routedPackets, expectedCount = 3)
 
                     val stateA = lobbyManager.getLobby(lobbyA)?.currentState()
                     val stateB = lobbyManager.getLobby(lobbyB)?.currentState()
@@ -2318,6 +2319,17 @@ class MainServerLobbyRoutingIntegrationTest {
     private suspend fun receiveAnyPayload(
         session: io.ktor.client.plugins.websocket.DefaultClientWebSocketSession,
     ): NetworkMessagePayload = receiveRelevantTestPayload(session)
+
+    private suspend fun waitUntilCounter(
+        counter: AtomicInteger,
+        expectedCount: Int,
+    ) {
+        withTimeout(5_000) {
+            while (counter.get() < expectedCount) {
+                delay(5)
+            }
+        }
+    }
 
     private suspend fun waitUntilProcessed(
         manager: LobbyManager,
